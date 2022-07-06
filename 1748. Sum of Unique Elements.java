5d45e74c8dafea0cc7c58f2bb947d54eb45752d4
Tuhin <tuhinmaji314@gmail.com>
2022-07-06 21:29:58 +0530

    -

---
+++ 1748. Sum of Unique Elements.java
@@ -0,0 +1,58 @@
+/*
+1748. Sum of Unique Elements
+Easy
+
+817
+
+18
+
+Add to List
+
+Share
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+*/
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        int sum=0;
+        for(int num:map.keySet()){
+            if(map.get(num)==1){
+                sum+=num;
+            }
+        }
+        return sum;
+    }
+}
